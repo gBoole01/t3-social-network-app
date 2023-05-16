072587d5a632806d93ee5e158fb2434839d80fe9
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-16 17:22:13 +0200

    feat: add Post persistance

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,7 +1,9 @@
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
+import type { FormEvent } from "react";
 import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
   if (!textArea) return;
@@ -22,9 +24,24 @@ const Form = () => {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
+  const createPost = api.post.create.useMutation({
+    onSuccess: () => {
+      setInputValue("");
+    },
+  });
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createPost.mutate({ content: inputValue });
+  }
+
   if (status !== "authenticated") return null;
   return (
-    <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.user?.image} />
         <textarea
