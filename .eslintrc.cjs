3b5d988844f513cad5e4e139c624965cf070cb3a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-16 19:24:16 +0200

    Revert "fix: disable prisma eslint conflicting rules"
    This reverts commit 703691565bc894b992c73e072db1016948ff7200.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,12 +29,6 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
-    "@typescript-eslint/restrict-plus-operands": "off",
   },
 };
 
