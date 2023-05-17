69c0cacc4532b26213b8721bbc2782864bf46542
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-17 12:22:17 +0200

    feat: add Profile page

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,7 +1,117 @@
-import React from "react";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import ErrorPage from "next/error";
+import Link from "next/link";
+import { VscArrowLeft } from "react-icons/vsc";
+import { api } from "~/utils/api";
+import { ssgHelper } from "~/server/api/ssgHelper";
+import IconHoverEffect from "~/components/IconHoverEffect";
+import ProfileImage from "~/components/ProfileImage";
+import InfinitePostList from "~/components/InfinitePostList";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+  const posts = api.post.infiniteProfileFeed.useInfiniteQuery(
+    { userId: id },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  if (profile == null || profile.name == null) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{`Social Network | ${profile.name}`}</title>
+      </Head>
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.postsCount}{" "}
+            {getPlural(profile.postsCount, "Post", "Posts")} -{" "}
+            {profile.followersCount}{" "}
+            {getPlural(profile.followersCount, "Follower", "Followers")} -{" "}
+            {profile.followingCount} Following
+          </div>
+        </div>
+        <FollowButton
+          isFollowing={profile.isFollowing}
+          userId={id}
+          onClick={() => null}
+        />
+      </header>
+      <main>
+        <InfinitePostList
+          posts={posts.data?.pages.flatMap((page) => page.posts)}
+          isError={posts.isError}
+          isLoading={posts.isLoading}
+          hasMore={posts.hasNextPage || false}
+          fetchNewPosts={posts.fetchNextPage}
+        />
+      </main>
+    </>
+  );
 };
 
-export default Profile;
+type FollowButtonProps = {
+  isFollowing: boolean;
+  userId: string;
+  onClick: () => void;
+};
+
+const FollowButton = ({ isFollowing, userId, onClick }: FollowButtonProps) => {
+  return <h1>Follow</h1>;
+};
+
+const pluralRules = new Intl.PluralRules();
+function getPlural(number: number, singular: string, plural: string) {
+  return pluralRules.select(number) === "one" ? singular : plural;
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
+) {
+  const id = context.params?.id;
+
+  if (id == null) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const ssg = ssgHelper();
+  await ssg.profile.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+}
+
+export default ProfilePage;
