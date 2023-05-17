0c0a5c91edb0f249db8610f4219d56841484a138
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-05-17 12:19:57 +0200

    feat: add profile route

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const profileRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input: { id }, ctx }) => {
+      const currentUserId = ctx.session?.user.id;
+      const profile = await ctx.prisma.user.findUnique({
+        where: { id: id },
+        select: {
+          name: true,
+          image: true,
+          _count: {
+            select: { followers: true, following: true, posts: true },
+          },
+          followers:
+            currentUserId == null
+              ? undefined
+              : { where: { id: currentUserId } },
+        },
+      });
+
+      if (profile == null) return;
+
+      return {
+        name: profile.name,
+        image: profile.image,
+        followersCount: profile._count.followers,
+        followingCount: profile._count.following,
+        postsCount: profile._count.posts,
+        isFollowing: profile.followers.length > 0,
+      };
+    }),
+});
